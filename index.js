7423244c37680e71cbaac20389d11de1260a9bfa
Pelle Wessman <pelle@kodfabrik.se>
2019-01-24 10:23:39 +0100

    Extract check functions to their own files

--- index.js
+++ index.js
@@ -4,82 +4,9 @@ const { promisify } = require('util');
 
 const readJson = promisify(require('read-package-json'));
 const readInstalled = promisify(require('read-installed'));
-const semver = require('semver');
-const semverIntersect = require('@voxpelli/semver-set').intersect;
 
-const checkPackageVersions = function (requiredDependencies, installedDependencies, optionalDependencies) {
-  const errors = [];
-  const notices = [];
-
-  Object.keys(requiredDependencies).forEach(dependency => {
-    const targetVersion = requiredDependencies[dependency];
-    const installedVersion = (installedDependencies[dependency] || {}).version;
-
-    if (!semver.validRange(targetVersion)) {
-      notices.push(dependency + ': Target version is not a semantic versioning range. Can\'t check');
-    } else if (!installedVersion) {
-      if (!optionalDependencies[dependency]) {
-        errors.push(dependency + ': Missing dependency');
-      }
-    } else if (!semver.satisfies(installedVersion, targetVersion)) {
-      errors.push(dependency + ': Invalid version, expected a ' + targetVersion);
-    }
-  });
-
-  return { errors, notices };
-};
-
-const checkEngineVersions = function (engines, requiredDependencies, installedDependencies) {
-  let engineKeys = Object.keys(engines);
-
-  if (!engineKeys.length) {
-    engines = {
-      node: '*',
-      npm: '*'
-    };
-    engineKeys = Object.keys(engines);
-  }
-
-  const errors = [];
-  const warnings = [];
-  const notices = [];
-
-  let finalIntersections = Object.assign({}, engines);
-
-  Object.keys(requiredDependencies).forEach(dependency => {
-    const dependencyEngines = (installedDependencies[dependency] || {}).engines || {};
-
-    engineKeys.forEach(engine => {
-      if (!dependencyEngines[engine]) {
-        warnings.push(dependency + ': Missing engine: ' + engine);
-      } else {
-        const intersection = semverIntersect(engines[engine], dependencyEngines[engine]);
-
-        if (!intersection) {
-          errors.push(dependency + ': Incompatible "' + engine + '" engine requirement: ' + dependencyEngines[engine]);
-          finalIntersections[engine] = false;
-        } else if (intersection !== engines[engine]) {
-          errors.push(dependency + ': Narrower "' + engine + '" engine requirement needed: ' + dependencyEngines[engine]);
-
-          if (finalIntersections[engine]) {
-            finalIntersections[engine] = semverIntersect(finalIntersections[engine], dependencyEngines[engine]);
-          }
-        }
-      }
-    });
-  });
-
-  Object.keys(finalIntersections).forEach(engine => {
-    const intersection = finalIntersections[engine];
-    if (!intersection) {
-      errors.push('Incompatible combined "' + engine + '" requirements.');
-    } else if (intersection !== engines[engine]) {
-      errors.push('Combined "' + engine + '" engine requirement needs to be narrower: ' + intersection);
-    }
-  });
-
-  return { errors, warnings, notices };
-};
+const checkPackageVersions = require('./lib/check-package-versions');
+const checkEngineVersions = require('./lib/check-engine-versions');
 
 const installedCheck = async function (path = '.', options = {}) {
   if (typeof path === 'object') {
