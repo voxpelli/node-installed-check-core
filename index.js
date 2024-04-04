2dd8c39b76efbe394a4160f6816500e98a33085f
Pelle Wessman <pelle@kodfabrik.se>
2024-04-04 16:58:59 +0200

    fix: export `ROOT` and the `WorkspaceSuccess` type

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { checkVersionRange, checkVersionRangeCollection } from './lib/check-version-range.js';
-export { installedCheck } from './lib/installed-check.js';
+export { installedCheck, ROOT } from './lib/installed-check.js';
 export { performInstalledCheck } from './lib/perform-installed-check.js';
