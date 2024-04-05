5f0ab6c8ade1f77f7784e7fc5b13b50e609cbfbf
Pelle Wessman <pelle@kodfabrik.se>
2024-04-05 18:15:41 +0200

    feat: add a `fix` option for autofixing (#134)

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 export type * from './lib/lookup-types.d.ts';
 
 export type { VersionRangeItem, VersionRangeOptions, VersionRangeResult, VersionRangesOptions } from './lib/check-version-range.js';
-export type { LookupOptions, WorkspaceSuccess } from './lib/installed-check.js';
-export type { InstalledChecks, InstalledCheckOptions, InstalledCheckResult } from './lib/perform-installed-check.js';
+export type { LookupOptions, WorkspaceSuccess, InstalledCheckResult } from './lib/installed-check.js';
+export type { InstalledChecks, InstalledCheckOptions, PerformInstalledCheckResult } from './lib/perform-installed-check.js';
 
 export { checkVersionRange, checkVersionRangeCollection } from './lib/check-version-range.js';
 export { installedCheck, ROOT } from './lib/installed-check.js';
